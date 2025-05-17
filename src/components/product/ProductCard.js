38871e3a0cc6b073546b8088a9e71e5e33a52b99
Abhishek Yadav <monuy8830@gmail.com>
2025-05-17 11:16:29 +0530

    Remove z-index from SendQueryModal in ProductCard for improved layering

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -283,7 +283,6 @@ const ProductCard = ({ product, attributes }) => {
       { queryModalOpen && (
         <SendQueryModal
         isOpen={queryModalOpen}
-        className='z-50'
         onClose={()=> setQueryModalOpen(false)}
         product = {product}
         />
